5f4d62b069afea54b895f8513590924e83e76972
senyoudev <younesmeskafe2018@gmail.com>
2025-12-06 17:54:59 +0100

    part 2 of day1

--- Day1/Day1.java
+++ Day1/Day1.java
@@ -57,12 +57,42 @@ class Day1 {
         return password;
     }
 
+    /**
+     * To address the part2, we actually don't care about the dial position, but about the number of times we cross the border
+     * of the dial. we count the number of times we cross the border and return it.
+     */
+    private static int findPassword_part2(List<String> lines) {
+        int password = 0;
+        int currentDialPosition = 50;
+
+        for (String line : lines) {
+            Optional<Move> maybeMove = Move.parse(line);
+            if (maybeMove.isPresent()) {
+                Move move = maybeMove.get();
+                int change = move.dir() == 'R' ? move.steps() : -move.steps();
+                int nextPosition = currentDialPosition + change;
+
+                if (change > 0) {
+                    password += Math.floorDiv(nextPosition, MAX_DIAL) - Math.floorDiv(currentDialPosition, MAX_DIAL);
+                } else {
+                    password += Math.floorDiv(currentDialPosition - 1, MAX_DIAL)
+                            - Math.floorDiv(nextPosition - 1, MAX_DIAL);
+                }
+
+                currentDialPosition = nextPosition;
+            }
+        }
+        return password;
+    }
+
     public static void main(String[] args) {
         Optional<List<String>> linesOptional = readInput("day1_input.txt");
 
         if (linesOptional.isPresent()) {
             int password = findPassword_part1(linesOptional.get());
+            int seCondPassword = findPassword_part2(linesOptional.get());
             System.out.println("Password Count (Part 1): " + password);
+            System.out.println("Password Count (Part 2): " + seCondPassword);
         } else {
             System.out.println("Could not run puzzle. Input file not available.");
         }
