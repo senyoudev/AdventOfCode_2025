133b6396b19f328e5fe85a615575a195558d1858
senyoudev <younesmeskafe2018@gmail.com>
2025-12-07 01:20:57 +0100

    part2 of day2

--- Day2/Day2.java
+++ Day2/Day2.java
@@ -27,24 +27,55 @@ public class Day2 {
             for(String idRange: IdRanges) {
                 String firstId = idRange.split("-")[0];
                 String secondId = idRange.split("-")[1];
-                // if the id is composed of some sequences repeated twice
                 for(long id = Long.parseLong(firstId); id <= Long.parseLong(secondId); id++) {
                     int length = String.valueOf(id).length();
                     // check if it's same value repeated
                     if(String.valueOf(id).substring(0, length/2).equals(String.valueOf(id).substring(length/2))) {
-                        System.out.println("Invalid Id: " + id);
                         sumOfInvalidIds += id;
                     }         
                 }
-
             }
-
         }
         return sumOfInvalidIds;
     }
 
+    private static boolean isValid(long id, int length) {
+        // Check if it's same value repeated twice or more
+        String s = String.valueOf(id);
 
+        for(int k = 1; k <= length / 2; k++) {
+            if(length % k == 0) {
+                int reps = length / k;
+                String elem = s.substring(0, k);
+                
+                String repeated = elem.repeat(reps);
+                if(repeated.equals(s)) {
+                    return true;
+                }
+            }
+        }
 
+        return false;
+    }
+
+    private static long findInvalidIds_part2(List<String> lines) {
+        long sumOfInvalidIds = 0;
+        for (String line : lines) {
+            String[] IdRanges = line.split(",");
+            for (String idRange : IdRanges) {
+                String firstId = idRange.split("-")[0];
+                String secondId = idRange.split("-")[1];
+                for (long id = Long.parseLong(firstId); id <= Long.parseLong(secondId); id++) {
+                    int length = String.valueOf(id).length();
+                    // check if it's same value repeated twice or more
+                    if(isValid(id, length)) {
+                        sumOfInvalidIds += id;
+                    }         
+                }
+            }
+        }
+        return sumOfInvalidIds;
+    }
 
     public static void main(String[] args) {
         Optional<List<String>> linesOptional = readInput("day2_input.txt");
